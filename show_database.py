9e51c76c6d0933395cbe2aed18f39681cf43093b
Kamila Wlodarska <k.wlodarska0@gmail.com>
2023-03-31 00:11:38 +0200

    change of data

--- show_database.py
+++ show_database.py
@@ -9,8 +9,8 @@ c = conn.cursor()
 
 #c.execute('''CREATE TABLE Zdarzenia(AId integer primary key autoincrement,'data','text','aktywny')''')
 
-#c.execute('''INSERT INTO Zdarzenia VALUES(1, '29.05.2019 12:10', 'xx', 'nie')''')
-#c.execute('''INSERT INTO Zdarzenia VALUES(2, '15.03.2023 16:55', 'yy', 'tak')''')
+#c.execute('''INSERT INTO Zdarzenia VALUES(1, '29.05.2019 12:10', 'task1', 'nie')''')
+#c.execute('''INSERT INTO Zdarzenia VALUES(2, '15.03.2023 16:55', 'task2', 'tak')''')
 
 for row in c.execute('''SELECT * FROM Zdarzenia'''):
     print(row)
